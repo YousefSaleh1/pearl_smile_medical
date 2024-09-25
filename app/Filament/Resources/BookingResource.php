609512d0a4e6d36f2c5f3c5b6e713a2dcc7f5e02
YousefSaleh1 <148710592+YousefSaleh1@users.noreply.github.com>
2024-09-25 22:15:48 +0300

    Service, Offer, Medical Team, Booking Filament Resiurce

---
+++ app/Filament/Resources/BookingResource.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BookingResource\Pages;
+use App\Filament\Resources\BookingResource\RelationManagers;
+use App\Models\Booking;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BookingResource extends Resource
+{
+    protected static ?string $model = Booking::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('service_id')
+                    ->relationship('service', 'name')
+                    ->required(),
+                Forms\Components\TextInput::make('name')
+                    ->label('Name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('email')
+                    ->label('Email')
+                    ->email()
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('phone_number')
+                    ->label('Phone Number')
+                    ->tel()
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('message')
+                    ->label('Message')
+                    ->required()
+                    ->maxLength(65535)
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('service.name')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->label('Email')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('phone_number')
+                    ->label('Phone Number')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('message')
+                    ->label('Message')
+                    ->words(5)
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label('Created At')
+                    ->searchable()
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->label('Updated At')
+                    ->searchable()
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                SelectFilter::make('service_id')
+                ->label('Service')
+                ->relationship('service', 'title_en')
+                ->searchable()
+                ->preload()
+                ->multiple(),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBookings::route('/'),
+            'view' => Pages\ViewBooking::route('/{record}'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/OfferResource.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\OfferResource\Pages;
+use App\Filament\Resources\OfferResource\RelationManagers;
+use App\Models\Offer;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
+
+class OfferResource extends Resource
+{
+    protected static ?string $model = Offer::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Card::make()
+                    ->schema([
+                        Forms\Components\Select::make('service_id')
+                            ->relationship('service', 'title_en')
+                            ->required(),
+                        Forms\Components\Repeater::make('images')
+                            ->label('Images')
+                            ->relationship('images')
+                            ->maxItems(2)
+                            ->schema([
+                                Forms\Components\FileUpload::make('path')
+                                    ->label('Upload Image')
+                                    ->preserveFilenames()
+                                    ->directory('image/Offers')
+                                    ->imageEditor()
+                                    ->getUploadedFileNameForStorageUsing(
+                                        fn(TemporaryUploadedFile $file): string => (string) str($file->getClientOriginalName())
+                                            ->prepend(now()->timestamp),
+                                    )
+                                    ->openable()
+                                    ->downloadable()
+                                    ->required(),
+
+                                Forms\Components\Grid::make(2) // Create a grid layout for alt text
+                                    ->schema([
+                                        Forms\Components\TextInput::make('alt_en')
+                                            ->label('Alt Text (English)')
+                                            ->minLength(2)
+                                            ->maxLength(100)
+                                            ->required(),
+
+                                        Forms\Components\TextInput::make('alt_ar')
+                                            ->label('Alt Text (Arabic)')
+                                            ->minLength(2)
+                                            ->maxLength(100)
+                                            ->required(),
+                                    ]),
+                            ])
+                            ->createItemButtonLabel('Add Image'),
+                    ])
+                    ->columnSpanFull()
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('service.title_en')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->searchable()
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->searchable()
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                SelectFilter::make('service_id')
+                ->label('Service')
+                ->relationship('service', 'title_en')
+                ->searchable()
+                ->preload()
+                ->multiple(),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListOffers::route('/'),
+            'create' => Pages\CreateOffer::route('/create'),
+            'view' => Pages\ViewOffer::route('/{record}'),
+            'edit' => Pages\EditOffer::route('/{record}/edit'),
+        ];
+    }
+}
