7731be02ad785131f532506c32271d3aba950ace
YousefSaleh1 <148710592+YousefSaleh1@users.noreply.github.com>
2024-09-28 13:02:55 +0300

    Delete Observer

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Filament\Notifications\Actions\Action;
+use Filament\Notifications\Notification;
 
 class Booking extends Model
 {
@@ -22,6 +24,28 @@ class Booking extends Model
         'phone_number',
         'message',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($booking) {
+            $recipient = User::where('email', 'admin@admin.com')->first();
+            Notification::make()
+                ->title('New Booking Added')
+                ->success()
+                ->body('A new booking was made by ' . $booking->name . ' for the service: ' . $booking->service->title_en . '.')
+                ->actions([
+                    Action::make('view')
+                        ->button()
+                        ->markAsRead(),
+                    Action::make('markAsUnread')
+                        ->button()
+                        ->markAsUnread(),
+                ])
+                ->sendToDatabase($recipient);
+        });
+    }
 
     /**
      * Get the service that owns the Booking
